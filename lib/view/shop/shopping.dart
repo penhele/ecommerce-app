9073cdc0bc0dac63536a063e95be1f6a64f452cf
penhele <stephenhelk@gmail.com>
2025-07-26 21:44:57 +0700

    feat: add shopping screen

--- lib/view/shop/shopping.dart
+++ lib/view/shop/shopping.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/app_textstyles.dart';
+import '../widgets/category_chips.dart';
+import '../widgets/filter_bottom_sheet.dart';
+import '../widgets/product_grid.dart';
+
 class ShoppingScreen extends StatelessWidget {
   const ShoppingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('shopping screen')));
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      appBar: AppBar(
+        title: Text(
+          'Shopping',
+          style: AppTextStyle.withColor(
+            AppTextStyle.h3,
+            isDark ? Colors.white : Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: isDark ? Colors.white : Colors.black,
+            ),
+          ),
+          IconButton(
+            onPressed: () => FilterBottomSheet.show(context),
+            icon: Icon(
+              Icons.filter_list,
+              color: isDark ? Colors.white : Colors.black,
+            ),
+          ),
+        ],
+      ),
+
+      body: const Column(
+        children: [
+          Padding(padding: EdgeInsets.only(top: 16), child: CategoryChips()),
+          Expanded(child: ProductGrid()),
+        ],
+      ),
+    );
   }
 }
